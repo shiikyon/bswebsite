e6646a5e14bec8df61345948d68682c0dfd8278b
shiiki <164728163+shiikyon@users.noreply.github.com>
2024-07-01 23:31:00 +0800

    Add files via upload

---
+++ bscript.js
@@ -0,0 +1,16 @@
+var modal = document.getElementById("login-popup");
+var btn = document.getElementById("login-link");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = none;
+    }
+}
